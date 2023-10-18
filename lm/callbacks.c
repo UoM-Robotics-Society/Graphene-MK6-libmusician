f2042633f82b18818e5959da4abc6150562e1b69
Bottersnike <bottersnike237@gmail.com>
2023-10-18 14:27:19 +0100

    Oops

--- lm/callbacks.c
+++ lm/callbacks.c
@@ -19,9 +19,9 @@ lm_do_control(uint32_t time, uint8_t channel, uint8_t control, uint8_t value) {
  */
 void __attribute__((weak))
 lm_do_note_on(uint32_t time, uint8_t channel, uint8_t note, uint8_t vel) {
-    lm_do_note_down(uint32_t time, uint8_t channel, uint8_t note, uint8_t vel);
+    lm_do_note_down(time, channel, note, vel);
 }
 void __attribute__((weak))
 lm_do_note_off(uint32_t time, uint8_t channel, uint8_t note, uint8_t vel) {
-    lm_do_note_up(uint32_t time, uint8_t channel, uint8_t note, uint8_t vel);
+    lm_do_note_up(time, channel, note, vel);
 }
